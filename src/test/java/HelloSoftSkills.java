e3a8ab177c4c374df0922efac5df65ef6d9ec069
Kainazar <82962567+Kainazar@users.noreply.github.com>
2021-05-26 19:28:36 -0500

    added 1 line

--- src/test/java/HelloSoftSkills.java
+++ src/test/java/HelloSoftSkills.java
@@ -1,5 +1,7 @@
 public class HelloSoftSkills {
     public static void main(String[] args) {
         System.out.println("Hello Soft Skills");
+        
+        System.out.println("Hello Friends!");
     }
 }
