3f67a4455dfbf1450be9981769ea9d9f9f12cdff
UnionTest-alt <2021softskills@gmail.com>
2021-06-02 23:16:36 -0500

    changes in line 3 and deleting line4

--- src/test/java/HelloSoftSkills.java
+++ src/test/java/HelloSoftSkills.java
@@ -1,7 +1,6 @@
 public class HelloSoftSkills {
     public static void main(String[] args) {
-        System.out.println("Hello Soft Skills");
-        
-        System.out.println("Hello Friends!");
+        System.out.println("Hello Soft Skills today is Friday 06/04/21");
+
     }
 }
