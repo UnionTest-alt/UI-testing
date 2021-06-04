f2cba30d1b539eb2ddf30c0c4e9eba26b1285759
Bakyt Kainazarov <bakyt.kainazar@gmail.com>
2021-06-04 17:02:00 -0500

    change

---
+++ UI-testing/src/main/java/EM_softskills_regression.java
@@ -0,0 +1,7 @@
+public class EM_softskills_regression {
+    public static void main(String[] args) {
+        System.out.println("HelloFromBakyt");
+        System.out.println();
+        System.out.println("********");
+    }
+}
